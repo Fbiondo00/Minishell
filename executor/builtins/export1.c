6ffda54bfc64fd70af835d965f11a30d804c5de6
Fbiondo420 <113358699+fla0000000@users.noreply.github.com>
2023-09-17 18:10:51 +0200

    wip.o

--- executor/builtins/export1.c
+++ executor/builtins/export1.c
@@ -6,7 +6,7 @@
 /*   By: flaviobiondo <flaviobiondo@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 18:59:32 by flaviobiond       #+#    #+#             */
-/*   Updated: 2023/09/14 17:41:41 by flaviobiond      ###   ########.fr       */
+/*   Updated: 2023/09/17 18:08:27 by flaviobiond      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,6 @@ void	ft_conc_2(t_shell *shell, char *str, int i)
 	shell->env = ft_realloc(shell->env, sizeof(char *) * (i + 2));
 	shell->env[i] = str1;
 	shell->env[i + 1] = 0;
-	// free(str1);
 }
 
 void	ft_conc(t_shell *shell, char *str, int y)
